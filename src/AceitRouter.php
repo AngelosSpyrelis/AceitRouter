8f72c8ee4c3324ce938e0380fc1741440915eb92
Angelos-Spyrelis <spyrelis.angelos@gmail.com>
2025-05-20 22:04:35 +0300

    Fixed typo

--- src/AceitRouter.php
+++ src/AceitRouter.php
@@ -3,16 +3,21 @@ namespace AceitDesign\Router;
 class AceitRouter {
     private $prefixes = [];
     private $suffixes = [];
+
+    private $middleware = [];
     private $fallback;
     private $routes = [];
 
     private $default;
 
-    private $pageErrorDefault;
-
+    private $isCaseSensitive;
     
-    public function __construct() {
-        
+    /**
+     * Summary of __construct
+     * @param bool $isCaseSensitive - Determines whether matching is case sensitive or not
+     */
+    public function __construct($isCaseSensitive = false) {
+        $this->isCaseSensitive = $isCaseSensitive;
     }
 
     /**
@@ -50,23 +55,6 @@ class AceitRouter {
     }
 
     /**
-     * Summary of setPageErrorDefault
-     * Sets the page error default callback. It is called when there is a page match but the provided callback is not callable.
-     * @param mixed $default - The default page callback (some kind of error page, usually.).
-     * @throws \InvalidArgumentException - If no valid callback is provided.
-     * @return void
-     */
-    public function setPageErrorDefault($default){
-        if(!is_callable($default)){
-            throw new \InvalidArgumentException(
-                "pageError must be callable. Received: " . 
-                (is_object($default) ? get_class($default) : gettype($default))
-            );
-        }
-        $this->pageErrorDefault = $default;
-    }
-
-    /**
      * Summary of setRoutes
      * Sets the routes that the router will use to match requests. Expects an array (nested, optionally) of callables
      * @param array $routes - An array of callables (Can be nested)
@@ -84,98 +72,112 @@ class AceitRouter {
         $this->routes = $routes;
     }
 
+
+    /**
+     * Summary of addMiddleware
+     * Adds middleware that can be chained in specific routes
+     * @param array $middleware - An array of callables.
+     * @throws \InvalidArgumentException
+     * @return static
+     */
+    public function addMiddleware(array $middleware){
+        foreach($middleware as $callback){
+            if(!is_callable($callback)){
+             throw new \InvalidArgumentException(
+                    "Middleware must be callable. Received: " . 
+                    (gettype($callback))
+                );
+            }
+            array_push($this->middleware, $callback);
+        }
+        return $this;
+    }
+
     /**
      * Summary of addPrefixes
      * Accepts a single callable or array of callables and adds them to the prefixes. (Callables called before the main matching function).
-     * @param mixed $prefixes - Array or single callable to be added.
+     * @param array $prefixes - Array of callables to be added.
      * @throws \InvalidArgumentException - If no valid callback is provided.
      * @return void
      */
     public function addPrefixes($prefixes){
-        if(is_array($prefixes)){
-            foreach($prefixes as $prefix){
-                if(!is_callable($prefix)){
-                    throw new \InvalidArgumentException(
-                        "Prefix must be callable. Received: " . 
-                        (gettype($prefix))
-                    );
-                }
-                array_push($this->prefixes, $prefix);
-            }
-        }
-        else{
-            if(!is_callable($prefixes)){
+        
+        foreach($prefixes as $prefix){
+            if(!is_callable($prefix)){
                 throw new \InvalidArgumentException(
                     "Prefix must be callable. Received: " . 
-                    (gettype($prefixes))
+                    (gettype($prefix))
                 );
             }
-            array_push($this->prefixes, $prefixes);
+            array_push($this->prefixes, $prefix);
         }
-        
     }
 
         /**
      * Summary of addPrefixes
      * Accepts a single callable or array of callables and adds them to the prefixes. (Callables called before the main matching function).
-     * @param mixed $prefixes - Array or single callable to be added.
+     * @param array $prefixes - Array of callables to be added.
      * @throws \InvalidArgumentException - If no valid callback is provided.
      * @return void
      */
     public function addSuffixes($suffixes){
-        if(is_array($suffixes)){
-            foreach($suffixes as $suffix){
-                if(!is_callable($suffix)){
-                    throw new \InvalidArgumentException(
-                        "Suffix must be callable. Received: " . 
-                        (gettype($suffix))
-                    );
-                }
-                array_push($this->suffixes, $suffixes);
-            }
-        }
-        else{
-            if(!is_callable($suffixes)){
+        
+        foreach($suffixes as $suffix){
+            if(!is_callable($suffix)){
                 throw new \InvalidArgumentException(
                     "Suffix must be callable. Received: " . 
-                    (gettype($suffixes))
+                    (gettype($suffix))
                 );
             }
-            array_push($this->suffixes, $suffixes);
+            array_push($this->suffixes, $suffix);
         }
         
     }
    
-
     /**
      * Summary of addRoute
      * Adds a route to the existing routes. Also, overwrites a route with the same name.
-     * @param mixed $route - The name of the route e.g. 'home'
-     * @param mixed $callback - The callback function that will correspond to the route.
-     * @param mixed $path - The path the route is supposed to go. If left empty, it will set the route to the root directory.
+     * @param array $path - The path the route is supposed to go. The last (or only) element should be the route name. 
+     * @param mixed $handler - The callback function that will correspond to the route.
+     * @param array $middleware - The callbacks that will be chained along with the handler.
      * @throws \InvalidArgumentException - If no valid callback is provided or an invalid path is given (In case of nested paths).
-     * @return void
      */
-    public function addRoute($route, $callback, $path = []){
-        if(!is_callable($callback)){
+    public function addRoute(array $path, $handler, array $middleware = []){
+        if(!is_callable($handler)){
             throw new \InvalidArgumentException(
                 "Route callback must be callable. Received: " . 
-                (gettype($callback))
+                (gettype($handler))
             );
         }
+
         $current = &$this->routes;
-        foreach($path as $segment){
-            if(!array_key_exists($segment, $current)){
-                throw new \InvalidArgumentException(
-                    "Route path does not exist. Route path provided: " . 
-                    (print_r($path))
-                );
+        for($i = 0; $i < count($path); $i++){
+            if(strpos($path[$i], '{') !== false){
+                $paramName = trim($path[$i], '{}');
+                $current['params'] = $paramName;
+               
+                continue;
             }
-            $current = &$current[$segment];
+            if(!array_key_exists($path[$i], $current)){
+                $current[$path[$i]] = [];
+            }
+            $current = &$current[$path[$i]];
         }
 
-        $current[$route]['callback'] = $callback;
-        return;
+        $current['handler'] = $handler;
+        $current['middleware'] = [];
+        foreach($middleware as $callback){
+            if(!is_callable($callback)){
+                throw new \InvalidArgumentException(
+                    "Route callback must be callable. Received: " . 
+                    (gettype($handler))
+                );
+            }
+            array_push($current['middleware'], $callback);
+        }        
+
+        return $this;
+        
     }
 
 
@@ -185,6 +187,7 @@ class AceitRouter {
      * Calls the prefix middleware.
      * Segments the request into parts separated by '/'
      * Tries to match each segment with the routes equivalent by checking to see if the segment exists as a key in the routes named array.
+     * Calls route middleware
      * Calls the suffix middleware.
      * Calls the various default or fallback callable in case of errors: 
      * 404: fallback
@@ -199,8 +202,10 @@ class AceitRouter {
             throw new \LogicException("No routes defined. Use setRoutes() or addRoute() first.");
         }
        
-        $_SERVER['REQUEST_URI'] = strtolower(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
-        echo $_SERVER['REQUEST_URI'];
+        if(!$this->isCaseSensitive){
+            $_SERVER['REQUEST_URI'] = strtolower(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
+        }
+        
         foreach ($this->prefixes as $prefix) {
             call_user_func($prefix);
         }
@@ -219,13 +224,23 @@ class AceitRouter {
         }
 
         $routesSearch = &$this->routes;
-        foreach ($requestSegments as $segment) {
-            if (is_array($routesSearch) && array_key_exists($segment, $routesSearch)) {
-                $routesSearch = &$routesSearch[$segment];
+        $params = [];
+        $lastValidPath = null;
+        for($i=0; $i < count($requestSegments); $i++){
+            
+            if (array_key_exists($requestSegments[$i], $routesSearch)) {
+                $routesSearch = &$routesSearch[$requestSegments[$i]];
             } else {
                 echo 'Fallback called';
                 call_user_func($this->fallback);
                 return;
+            }
+            if(array_key_exists('handler', $routesSearch)){
+                $lastValidPath = $routesSearch;
+            }
+            if(array_key_exists('params', $routesSearch)){
+                $i++;
+                $params[$routesSearch['params']]=$requestSegments[$i];
             }
         }
 
@@ -233,17 +248,21 @@ class AceitRouter {
             call_user_func($suffix);
         }
 
-        if (!is_callable($routesSearch['callback'])) {
+        if (!is_callable($lastValidPath['handler'])) {
             error_log("[AceitRouter] ".date('Y-m-d H:i:s')." Provided callback is not callable");
-            if(is_callable($this->default)){
-                call_user_func($this->default);
+            if(is_callable($this->fallback)){
+                call_user_func($this->fallback);
             }
             else{
                 echo "<h1>There is an issue with the page you are trying to access.</h1>";
             }
             return;
         }
-        call_user_func($routesSearch['callback']);
+        // 3. Run route-specific middleware
+        foreach ($lastValidPath['middleware'] as $middleware) {
+            call_user_func($middleware);
+        }
+        call_user_func($lastValidPath['handler'], $params);
         return;
         
     }
